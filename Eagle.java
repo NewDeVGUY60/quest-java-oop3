4ff84edc5530fabdcfc5d433a75762c8c88efae1
NewDeVGUY60 <[guynicolas60@gmail.com](mailto:guynicolas60@gmail.com)>
2023-02-28 14:51:28 +0100

    end

--- Eagle.java
+++ Eagle.java
@@ -1,4 +1,4 @@
-public class Eagle extends Bird {
+public class Eagle extends Bird implements Fly {
 
     private boolean flying;
     private int altitude;
@@ -21,4 +21,58 @@ public class Eagle extends Bird {
     public String sing() {
         return "Screech!";
     }
+
+    // @Override
+    public void takeOff(){
+        if(!this.flying && this.altitude ==0){
+            this.flying = true;
+            System.out.println(this.getName()+" takes off in the sky.");
+        }
+    }
+
+    //  @Override
+    public int ascend(int meters) {
+        if(this.flying){
+            this.altitude = this.altitude + meters;
+            System.out.println(this.getName()+" flies upward, altitude : " + this.getAltitude());
+            
+        }
+        return  this.altitude ;
+    }
+
+    // @Override
+    public int descend(int meters) {
+        if(this.flying){
+            this.altitude = Math.max(this.altitude - meters,0);
+            System.out.println(this.getName()+" flies downward, altitude : " + this.getAltitude());
+            
+        }
+        return  this.altitude ;
+    }
+    // @Override
+    public void glide(){
+        if(this.flying){
+            System.out.println("It glides into the air.");
+        }
+        
+    }
+    // @Override
+    public void land(){
+        if(this.flying && this.altitude <= 1){
+            System.out.printf("%s lands on the ground.%n", this.getName());
+        } else {
+            System.out.printf("%s is too high, it can't lands.%n", this.getName());
+        }
+    }
+
+
 }
+
+// $ Eye Cherry takes off in the sky.
+// $ Eye Cherry flies upward, altitude : 120
+// $ Eye Cherry flies upward, altitude : 150
+// $ It glides into the air.
+// $ Eye Cherry flies downward, altitude : 10
+// $ Eye Cherry is too high, it can't lands.
+// $ Eye Cherry flies downward, altitude : 1
+// $ Eye Cherry lands on the ground.
---
+++ Fly.java
@@ -0,0 +1,11 @@
+public interface Fly {
+
+    void land();
+    int ascend(int meters);
+    int descend(int meters);
+    void glide();
+    void takeOff();
+
+
+}
+
